8b9b4ae3a4c2c532daa0ed553cc4771f37aa4070
userc8e <caitlynquack@gmail.com>
2025-10-04 19:37:29 -0400

    implemented ThingList; vars, constructor, addThing()

--- ThingList.java
+++ ThingList.java
@@ -1,3 +1,22 @@
 public class ThingList {
+    Node head; //first Node in the list
+
+    public ThingList() {
+        head = null;
+    }
+
+    // adds a new Node into the list
+    public void addThing(Node n) {
+        if (head == null) {
+            head = n;
+        } else {
+            Node current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(n); 
+        }
+    }
+
     
 }
