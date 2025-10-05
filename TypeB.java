29794e2a3affa3fc1ac9c734cedbcbd05ba4e60a
userc8e <caitlynquack@gmail.com>
2025-10-05 19:13:21 -0400

    implemented TypeB maybeTurn()

--- TypeB.java
+++ TypeB.java
@@ -1,3 +1,26 @@
-public class TypeB {
-    
+import java.util.Random;
+
+public class TypeB extends Thing {
+    public int timeSinceLast;
+
+    public TypeB(int row, int col, char lab) {
+        super(row, col, lab);
+        timeSinceLast = 0;
+    }
+
+    @Override
+    public void maybeTurn(Random rand) {
+        int i = rand.nextInt(3);
+        timeSinceLast++;
+         if (timeSinceLast == 10) {
+            timeSinceLast = 0;
+            if (i == 1) {
+               super.rightTurn();
+            }
+
+            if (i == 2) {
+               super.leftTurn();
+            }
+         }
+    }
 }
