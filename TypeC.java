8bb5880c46e6793ab857a281f0cd50c241ef9c4d
userc8e <caitlynquack@gmail.com>
2025-10-05 19:18:48 -0400

    added signatures for constructor and maybeTurn()

---
+++ TypeC.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class TypeC extends Thing {
+    public TypeC(int row, int col, char lab) {
+        super(row, col, lab);
+    }
+
+    @Override
+    public void maybeTurn(Random rand) {
+        //TO DO
+    }
+}
