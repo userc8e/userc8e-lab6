9cfa6d41bfbeaff4311e3754d6a7c89a6bb5d1ac
userc8e <caitlynquack@gmail.com>
2025-10-05 19:06:04 -0400

    implemented TypeA maybeTurn()

--- TypeA.java
+++ TypeA.java
@@ -1,3 +1,18 @@
-public class TypeA {
-    
+import java.util.Random;
+public class TypeA extends Thing {
+    public TypeA(int row, int col, char lab) {
+        super(row, col, lab);
+    }
+
+    @Override
+    public void maybeTurn(Random rand) {
+        int i = rand.nextInt(3);
+        if (i == 1) {
+            super.rightTurn();
+        }
+
+        if (i == 2) {
+            super.leftTurn();
+        }
+    }
 }
